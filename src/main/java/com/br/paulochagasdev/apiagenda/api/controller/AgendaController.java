61914ae3b5160f9459f3ccc4063d6e4d3235cc26
PauloRicardoDev <pauloricardochagas.dev@gmail.com>
2024-12-30 01:27:09 -0300

    Ajustando documentação com swagger.

--- src/main/java/com/br/paulochagasdev/apiagenda/api/controller/AgendaController.java
+++ src/main/java/com/br/paulochagasdev/apiagenda/api/controller/AgendaController.java
@@ -5,12 +5,12 @@ import com.br.paulochagasdev.apiagenda.api.request.AgendaRequest;
 import com.br.paulochagasdev.apiagenda.api.response.AgendaResponse;
 import com.br.paulochagasdev.apiagenda.domain.entity.Agenda;
 import com.br.paulochagasdev.apiagenda.domain.service.AgendaService;
-import com.br.paulochagasdev.apiagenda.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,28 +22,31 @@ public class AgendaController {
     private final AgendaService service;
     private final AgendaMapper mapper;
 
-    @PostMapping
-    public ResponseEntity<AgendaResponse> salvar(@RequestBody AgendaRequest request){
+    @PostMapping("/salvar")
+    public ResponseEntity<AgendaResponse> salvar(@Valid @RequestBody AgendaRequest request) {
 
         Agenda agenda = mapper.toAgenda(request);
         Agenda agendaSalva = service.salvar(agenda);
         AgendaResponse agendaResponse = mapper.toAgendaResponse(agendaSalva);
         return ResponseEntity.status(HttpStatus.OK).body(agendaResponse);
+
     }
 
     @GetMapping("/listar")
     public ResponseEntity<List<AgendaResponse>> listar() {
+
         List<Agenda> agendas = service.listarTodos();
         List<AgendaResponse> responseList = mapper.toAgendaResponseList(agendas);
         return ResponseEntity.status(HttpStatus.OK).body(responseList);
+
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<AgendaResponse> buscar(@PathVariable Long id){
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<AgendaResponse> buscar(@PathVariable Long id) {
 
         Optional<Agenda> opt_agenda = service.buscar(id);
 
-        if (opt_agenda.isEmpty()){
+        if (opt_agenda.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
 
